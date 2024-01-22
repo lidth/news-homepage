b6ffc16695cf537845799151b6ce861e57510c4e
Random <155200367+lidth@users.noreply.github.com>
2024-01-22 18:07:28 -0300

    menu itens js

--- assets/jos.js
+++ assets/jos.js
@@ -1,7 +1,13 @@
-
-
-
 function menu() {
+    let menuitens = document.querySelector('div#menu-itens')
     let menuimg = document.querySelector('img#menu')
+    let ishidden = menuitens.style.display == '' || menuitens.style.display == 'none'
+
+    if (ishidden){
     menuimg.src = 'assets/images/icon-menu-close.svg'
+    menuitens.style.display = 'block'
+    } else {
+        menuimg.src = 'assets/images/icon-menu.svg'
+        menuitens.style.display = 'none'
+    }
 }
